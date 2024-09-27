26547c0028636d2a66396b80de1c44219210853f
Oraclegods <58555206+Oraclegods@users.noreply.github.com>
2024-09-27 13:02:01 +0000

    I added prep 3 workload

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+         string playAgain;
+
+        do
+        {
+            // Initialize random number generator and generate a magic number between 1 and 100
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guess = 0;
+            int numberOfGuesses = 0;
+
+            Console.WriteLine("Welcome to the 'Guess My Number' game!");
+
+            // Loop until the user guesses the correct number
+            while (guess != magicNumber)
+            {
+                // Ask the user for their guess
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                // Determine if the guess is higher, lower, or correct
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+                }
+            }
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
-}
+    }
