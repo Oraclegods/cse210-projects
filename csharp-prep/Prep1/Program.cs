fe9a84f7b0f9bb88951c5c5bc7f56a734c82d5d9
Oraclegods <58555206+Oraclegods@users.noreply.github.com>
2024-09-27 04:28:51 +0000

    I added new program that asks for names

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
+    // asking a uer for their names
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
