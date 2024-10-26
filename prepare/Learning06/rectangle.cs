6598badcc4c72dc266bf7ed2b2517b4844bd1746
Oraclegods <58555206+Oraclegods@users.noreply.github.com>
2024-10-26 12:32:54 +0000

    rectangle update

---
+++ prepare/Learning06/rectangle.cs
@@ -0,0 +1,21 @@
+// Rectangle.cs
+using System;
+
+public class Rectangle : Shape
+{
+    private double _width;
+    private double _height;
+
+    // Constructor
+    public Rectangle(string color, double width, double height) : base(color)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    // Override GetArea method
+    public override double GetArea()
+    {
+        return _width * _height;
+    }
+}
